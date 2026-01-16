c89b0c905b87a3bded73f0a5a7ae79f1f405e1fe
Fagogenis99 <sdi2100199@di.uoa.gr>
2026-01-16 20:22:47 +0200

    updating the communication pipeline

---
+++ kernel/pstat.h
@@ -0,0 +1,15 @@
+#ifndef _PSTAT_H_
+#define _PSTAT_H_
+
+#include "param.h" // for NPROC
+
+struct pstat{
+    int inuse[NPROC]; // whether this slot of the process table is in use (1 or 0)
+    int pid[NPROC];   // the PID of each process
+    int priority[NPROC]; // the priority level of each process
+    int state[NPROC]; // the state of each process
+    int ticks_used[NPROC]; // ticks used at current level
+    int ticks_waiting[NPROC]; // ticks spent waiting in the current queue
+};
+
+#endif // _PSTAT_H_
